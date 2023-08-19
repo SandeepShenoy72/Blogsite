4ea7b325c8edece60e7e2010375ddf9a4a7e8268
Sandeep Shenoy Sirsikar <sshenoys@microsoft.com>
2023-08-19 18:21:26 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express"
+
+const app =express()
+
+app.use(express.urlencoded({ extended: true }));
+
+const PORT=3000
+
+app.use(express.static('public'))
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Listening on ${PORT}`)
+})
